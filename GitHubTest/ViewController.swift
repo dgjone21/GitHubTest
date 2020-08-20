d239ebf03c2fa480bf6c7ce3fd39ee9d0822df74
Dalen Jones <dgjone21@gmail.com>
2020-08-20 14:52:31 -0400

    Did this work?

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Dalen Jones on 8/20/20.
+//  Copyright © 2020 Dalen Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      print("This is a Git tutorial")
+    }
+
+
+}
+
